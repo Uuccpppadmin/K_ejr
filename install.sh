92f61b93b0d96d840bd6b4a32b07370476f13117
Uuccpppadmin <mokjh773@gmail.com>
2025-07-03 03:02:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "📦 Installing and running K_ejr tool..."
+echo "📦 Installing K_ejr tool..."
 
 DEST_PATH="$PREFIX/bin/K_ejr"
 curl -s -o "$DEST_PATH" "https://raw.githubusercontent.com/Uuccpppadmin/K_ejr/main/K_ejr.py"
 chmod +x "$DEST_PATH"
 ln -sf "$DEST_PATH" "$PREFIX/bin/hhhh"
 
-echo "✅ Installed! Running tool now..."
-
-hhhh
+echo "✅ Installed! You can now run the tool by typing: hhhh"
